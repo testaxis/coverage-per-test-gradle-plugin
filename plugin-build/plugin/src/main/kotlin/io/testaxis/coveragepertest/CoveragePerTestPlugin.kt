1508af463aeb65d5d02cf173e36a0cbd59e06317
Casper Boone <mail@casperboone.nl>
2020-11-18 18:05:03 +0100

    Add runtime dependency by default

--- plugin-build/plugin/src/main/kotlin/io/testaxis/coveragepertest/CoveragePerTestPlugin.kt
+++ plugin-build/plugin/src/main/kotlin/io/testaxis/coveragepertest/CoveragePerTestPlugin.kt
@@ -2,19 +2,33 @@ package io.testaxis.coveragepertest
 
 import org.gradle.api.Plugin
 import org.gradle.api.Project
+import org.gradle.api.plugins.ApplicationPlugin
 
 const val EXTENSION_NAME = "coveragePerTestConfig"
 const val TASK_NAME = "coveragePerTest"
+const val RUNTIME_VERSION = "0.0.1"
 
 abstract class CoveragePerTestPlugin : Plugin<Project> {
     override fun apply(project: Project) {
-        // Add the 'template' extension object
+        project.plugins.apply(ApplicationPlugin::class.java)
+
         val extension = project.extensions.create(EXTENSION_NAME, CoveragePerTestExtension::class.java, project)
 
-        // Add a task that uses configuration from the extension object
         project.tasks.register(TASK_NAME, CoveragePerTestTask::class.java) {
             it.inputDirectory.set(extension.inputDirectory)
             it.outputDirectory.set(extension.outputDirectory)
         }
+
+        addRuntimeDependency(project)
+    }
+
+    private fun addRuntimeDependency(project: Project) {
+        project.repositories.maven {
+            it.setUrl("https://dl.bintray.com/casperboone/testaxis")
+        }
+
+        project.configurations.getAt("testRuntimeOnly").dependencies.add( // possibly only runtime?
+            project.dependencies.create("io.testaxis:coverage-per-test-runtime:$RUNTIME_VERSION")
+        )
     }
 }
